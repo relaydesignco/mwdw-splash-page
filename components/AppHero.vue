6edb3bb34783c46721fa3609f11a9eb0a02174d3
David Soards <davidysoards@gmail.com>
2020-07-28 23:59:53 -0400

    using padding instead of flex-column for hero section

--- components/AppHero.vue
+++ components/AppHero.vue
@@ -1,29 +1,26 @@
 <template>
-  <header class="h-screen p-4 md:p-12 flex flex-col justify-between relative">
-    <img src="/MidwestDesignWeek.svg" alt="Midwest Design Week" class="w-5/6" />
+  <header class="px-4 py-4 md:p-12 relative">
     <div class="absolute top-0 right-0 w-2/12">
       <svg-symbol />
     </div>
-    <div class="flex justify-between flex-wrap leading-tight items-center">
-      <h1 class="text-2xl md:text-3xl lg:text-4xl uppercase tracking-widest mb-8 xl:mb-0">
-        A Virtual Design Conference_
-      </h1>
-      <time class="text-2xl md:text-3xl lg:text-4xl font-semibold tracking-wider"
-        >09/21 - 25/2020</time
-      >
-    </div>
-
-    <div class="flex justify-around items-center md:w-3/4 lg:w-7/12 flex-wrap mx-auto">
-      <ul class="uppercase text-lg tracking-wider font-semibold">
+    <img src="/MidwestDesignWeek.svg" alt="Midwest Design Week" class="w-6/12" />
+    <h1 class="text-xl md:text-3xl lg:text-4xl uppercase leading-tight tracking-widest mt-4 mb-4">
+      A Virtual Design Conference_
+    </h1>
+    <time class="text-3xl lg:text-5xl font-semibold tracking-wider block mb-8 md:mb-20">
+      09/21 - 25/2020
+    </time>
+    <div class="flex justify-around items-center md:w-3/4 lg:w-7/12 flex-wrap mx-auto md:mb-12">
+      <ul class="uppercase text-lg md:text-xl tracking-wider font-semibold">
         <li>Louisville</li>
         <li>Cincinnati</li>
         <li>Toledo</li>
         <li>Indianapolis</li>
       </ul>
-      <div class="w-32">
+      <div class="w-32 md:w-48">
         <svg-logo />
       </div>
-      <img src="/MWDW2020.svg" alt="#MWDW2020" class="mt-10 md:mt-0 w-64" />
+      <img src="/MWDW2020.svg" alt="#MWDW2020" class="mt-10 md:mt-0 w-48 md:w-64" />
     </div>
   </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <div>
     <app-hero></app-hero>
     <main
-      class="px-4 md:px-12 lg:px-0 pt-24 pb-16 md:pb-32 max-w-screen-lg mx-auto md:grid grid-cols-2 gap-16"
+      class="px-4 md:px-12 lg:px-0 pt-12 pb-16 md:pb-32 max-w-screen-lg mx-auto md:grid grid-cols-2 gap-16"
     >
       <div>
         <h2 class="text-3xl mb-1 uppercase font-semibold">New This Year</h2>
-        <p class="text-xl mb-8">
+        <p class="text-xl mb-12">
           Our world is shaped by <strong>culture</strong> in its many forms: customary beliefs,
           shared values, learned behavior, and traditions, to name a few. The
           <strong>diversity</strong> of experience and culture is what makes each person unique,
@@ -14,14 +14,14 @@
           <strong>Louisville, Cincinnati, Toledo, and Indianapolis</strong>
           celebrate the many colorful voices of our communities.
         </p>
-        <h2 class="text-3xl mb-1 uppercase font-bold leading-tight">
+        <h2 class="text-3xl mb-8 md:mb-0 uppercase font-bold leading-tight">
           Save the Date
           <br />
           September 21 - 25, 2020
         </h2>
       </div>
       <div>
-        <h2 class="text-3xl mb-4 uppercase font-semibold mt-8 md:mt-0">Stay In Touch</h2>
+        <h2 class="text-3xl mb-4 uppercase font-semibold">Stay In Touch</h2>
         <email-form
           form-action="https://aiga.us3.list-manage.com/subscribe/post"
           mc-u="751e598f443a47697e8b78cb9"
